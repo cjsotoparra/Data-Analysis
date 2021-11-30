9c0ecc341f9b6080f8a164e175902c4352b94a87
Christian Soto <christian.j.sotoparra@gmail.com>
2021-11-29 19:54:18 -0700

    Making a simple webpage

---
+++ 0-simple-app.py
@@ -0,0 +1,15 @@
+import justpy as jp
+
+#Create simple webpage
+def app():
+
+	#create
+	wp = jp.QuasarPage()
+
+	#create 2 elements and connect them to webpage
+	h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
+	p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
+	return wp
+
+#call app function
+jp.justpy(app)
