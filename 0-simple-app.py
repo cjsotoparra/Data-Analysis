f67164c35b761a649f88474170e04e6d93ad4920
Christian Soto <christian.j.sotoparra@gmail.com>
2021-11-29 20:50:50 -0700

    Making a data visualization web app

--- 0-simple-app.py
+++ 0-simple-app.py
@@ -6,7 +6,7 @@ def app():
 	#create
 	wp = jp.QuasarPage()
 
-	#create 2 elements and connect them to webpage
+	#create 2 elements and add them to our component
 	h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 text-center q-pa-md")
 	p1 = jp.QDiv(a=wp, text="These graphs represent course review analysis")
 	return wp
